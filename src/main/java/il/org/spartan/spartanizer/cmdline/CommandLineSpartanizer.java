820afd7349214d29f008e8c936ee52ab6357f86e
M4tt30rru <matt.orru@gmail.com>
2016-10-30 20:52:31 +0200

    added switch to select different applicators

--- src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSpartanizer.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineSpartanizer.java
@@ -8,6 +8,8 @@ import java.io.*;
  * @since 2016 */
 public class CommandLineSpartanizer extends AbstractCommandLineSpartanizer {
   private final String name;
+  private boolean commandLineApplicator=false;
+  private boolean collectApplicator=true;
 
   CommandLineSpartanizer(final String path) {
     this(path, system.folder2File(path));
@@ -21,18 +23,31 @@ public class CommandLineSpartanizer extends AbstractCommandLineSpartanizer {
   @Override public void apply() {
     System.out.println(inputPath);
     try {
-      Reports.initializeFile(folder + name + ".before.java", "before");
-      Reports.initializeFile(folder + name + ".after.java", "after");
-      Reports.intializeReport(folder + name + ".CSV", "metrics");
-      Reports.intializeReport(folder + name + ".spectrum.CSV", "spectrum");
-     
-      CommandLineApplicator.defaultApplicator().passes(20)
-          .selection(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(inputPath))).go();
       
-      Reports.close("metrics");
-      Reports.close("spectrum");
-      Reports.closeFile("before");
-      Reports.closeFile("after");
+      if(collectApplicator){
+        Reports.initializeReport(folder + name + ".tips.CSV", "tips");
+        CollectApplicator.defaultApplicator()
+                         .selection(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(inputPath)))
+                         .go();
+        Reports.close("tips");
+        System.err.println("CollectApplicator: " + "Done!");
+      }
+      
+      if(commandLineApplicator){
+        Reports.initializeFile(folder + name + ".before.java", "before");
+        Reports.initializeFile(folder + name + ".after.java", "after");
+        Reports.initializeReport(folder + name + ".CSV", "metrics");
+        Reports.initializeReport(folder + name + ".spectrum.CSV", "spectrum");
+        
+        CommandLineApplicator.defaultApplicator().passes(20)
+            .selection(CommandLineSelection.of(CommandLineSelection.Util.getAllCompilationUnit(inputPath))).go();
+        
+        Reports.close("metrics");
+        Reports.close("spectrum");
+        Reports.closeFile("before");
+        Reports.closeFile("after");
+        System.err.println("commandLineApplicator: " + "Done!");
+      }
     } catch (final IOException x) {
       x.printStackTrace();
     }
